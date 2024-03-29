0c49133e5b00569952b14e11949df1d80e31ee55
ttasjwi <ttasjwi920@gmail.com>
2024-03-29 11:49:28 +0900

    주문 도메인 이벤트 구현

---
+++ order-service/order-domain/order-domain-core/src/main/kotlin/com/food/ordering/system/order/service/domain/event/OrderCancelledEvent.kt
@@ -0,0 +1,6 @@
+package com.food.ordering.system.order.service.domain.event
+
+import com.food.ordering.system.order.service.domain.entity.Order
+import java.time.ZonedDateTime
+
+class OrderCancelledEvent(order: Order, createdAt: ZonedDateTime) : OrderEvent(order, createdAt)
